d3869a5d9f9493cecfd39fbdf27597b3aac76469
张耀武 <zhang_yao_wu1993@yeah.net>
2016-08-25 17:32:38 +0800

    spring config init

---
+++ tc-platform/src/main/java/com/ysu/zyw/tc/web/TcMainController.java
@@ -0,0 +1,32 @@
+package com.ysu.zyw.tc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TcMainController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView idx() {
+        return index();
+    }
+
+    @RequestMapping(value = "index")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/not_found")
+    public ModelAndView resourceNotFoundPage() {
+        // TODO
+        return new ModelAndView("1");
+    }
+
+    @RequestMapping(value = "server_error")
+    public ModelAndView internalServerErrorPage() {
+        // TODO
+        return new ModelAndView("2");
+    }
+
+}
