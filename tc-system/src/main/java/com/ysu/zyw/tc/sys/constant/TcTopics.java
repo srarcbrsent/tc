038404e0bf7d0b234701bf52114db5d1968c75a3
张耀武 <zhang_yao_wu1993@yeah.net>
2016-10-22 22:30:43 +0800

    queue (1)

---
+++ tc-system/src/main/java/com/ysu/zyw/tc/sys/constant/TcTopics.java
@@ -0,0 +1,21 @@
+package com.ysu.zyw.tc.sys.constant;
+
+public class TcTopics {
+
+    public static abstract class Topic {
+
+        public static final String TOPIC_ACCOUNT = "topic_account";
+
+    }
+
+    public static abstract class TopicAttr {
+
+        public static final String CREATE = "CREATE";
+
+        public static final String DELETE = "DELETE";
+
+        public static final String UPDATE = "UPDATE";
+
+    }
+
+}
