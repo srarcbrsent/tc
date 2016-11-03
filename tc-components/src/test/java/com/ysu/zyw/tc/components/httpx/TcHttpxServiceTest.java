d7df733b447fd7257c04e47fc5479d229473071f
张耀武 <zhang_yao_wu1993@yeah.net>
2016-11-03 11:20:04 +0800

    httpx (5)

--- tc-components/src/test/java/com/ysu/zyw/tc/components/httpx/TcHttpxServiceTest.java
+++ tc-components/src/test/java/com/ysu/zyw/tc/components/httpx/TcHttpxServiceTest.java
@@ -1,6 +1,5 @@
 package com.ysu.zyw.tc.components.httpx;
 
-import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.ysu.zyw.tc.base.tools.TcIdWorker;
@@ -12,15 +11,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.MultiValueMap;
 
 import javax.annotation.Resource;
 import java.beans.IntrospectionException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -132,50 +128,6 @@ public class TcHttpxServiceTest {
         );
         MultiValueMap<String, String> multiValueMap7 = tcHttpxService.copy2MultiValueMap(orange);
         System.out.println(multiValueMap7);
-    }
-
-    /**
-     * liyunfei_zdd1 acd2031e987e44b09b4f8a033d943c81
-     */
-    @Test
-    public void testPushCoupon() {
-        for (int i = 0; i < 120; i++) {
-            pushCoupon();
-        }
-    }
-
-    private void pushCoupon() {
-        final String PROMOTIONX_CENTER_BASE = "http://promotionx.ptdanlu.com";
-        final String COUPON_NAME = "耀武赠送红包-" + (RandomUtils.nextInt(1000));
-        final String ACTIVITY_NAME = "耀武赠送红包-" + (RandomUtils.nextInt(1000));
-        final int COUPON_AMOUNT = 500 + RandomUtils.nextInt(5000);
-        final String COUPON_CREATE_PERSON = "c5f20a6943e6414181ee1b0ce9180c6d";
-        final String COUPON_RECEIVE_PERSON_LIST = "acd2031e987e44b09b4f8a033d943c81";
-        final int COUPON_NUM = 1;
-
-        // create coupon
-        JSONObject jsonObject1 = new JSONObject();
-        jsonObject1.put("couponName", COUPON_NAME);
-        jsonObject1.put("couponTypeId", "01");
-        jsonObject1.put("validDay", String.valueOf(3 + RandomUtils.nextInt(15)));
-        List<Object> amoutsList = new ArrayList<>();
-        JSONObject amouts = new JSONObject();
-        amouts.put("amt", COUPON_AMOUNT);
-        amouts.put("amount", COUPON_NUM);
-        amoutsList.add(amouts);
-        jsonObject1.put("amoutsList", amoutsList);
-        jsonObject1.put("createPerson", COUPON_CREATE_PERSON);
-        jsonObject1.put("createPersonName", "system");
-
-        // call
-        ResponseEntity<Map<String, Object>> responseEntity1 = tcHttpxService.postJson4Entity(
-                PROMOTIONX_CENTER_BASE + "/coupon/create",
-                null,
-                null,
-                jsonObject1,
-                new ParameterizedTypeReference<Map<String, Object>>() {
-                }
-        );
     }
 
 }
