dc4c52b3025e56ab2d08499dd4a06d75187e86b6
张耀武 <zhang_yao_wu1993@yeah.net>
2016-12-06 21:45:57 +0800

    project optimize (83)

--- tc-node/TcReplacer.js
+++ tc-node/TcReplacer.js
@@ -1,15 +1,7 @@
-var http = require('http');
+const fs = require('fs');
 
-http.createServer(function (request, response) {
-
-    // 发送 HTTP 头部
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+fs.readFile(__filename, (err, data) => {
+    if (err)
+        throw err;
+    console.log(data.toString());
+});
