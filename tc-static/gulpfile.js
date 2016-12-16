8988b1dd554f87bc33c2ba00b4a0c948d367972c
张耀武 <zhang_yao_wu1993@yeah.net>
2016-12-16 23:39:00 +0800

    project optimize (108)

--- tc-static/gulpfile.js
+++ tc-static/gulpfile.js
@@ -2,27 +2,45 @@
 // [npm install --save-dev gulp] -> 核心
 // [npm install --save-dev jshint] -> JS校验核心
 // [npm install --save-dev gulp-jshint] -> JS校验
-// [npm install --save-dev gulp-content-includer] ->
+// [npm install --save-dev gulp-file-include] -> HTML合并
+// [npm install --save-dev gulp-replace] -> 替换文本
 
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var fileinclude = require('gulp-file-include');
+var replace = require('gulp-replace');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     // 将你的默认的任务代码放在这
 });
 
 // 检查脚本
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     gulp.src('src/resources/scripts/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('fileinclude', function () {
+    gulp.src(['src/**/*.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@root'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('replace', function(){
+    gulp.src(['src/**/*.html'])
+        .pipe(replace(/\$\{staticBase}/g, 'http://static.tc.com'))
+        .pipe(gulp.dest('dist'));
+});
+
+
+
 // gulp-concat 合并js
 // gulp-concat-css 合并css
-
-//  合并html
 
 // gulp-htmlmin 压缩html
 // gulp-uglify 压缩丑化js
