9658207ee457c0f6404c0919edd19aa6d7787e70
张耀武 <zhang_yao_wu1993@yeah.net>
2016-12-21 17:26:22 +0800

    project optimize (130)

--- tc-apiimpl/src/main/java/com/ysu/zyw/tc/api/dao/TcDataDirectory.java
+++ tc-apiimpl/src/main/java/com/ysu/zyw/tc/api/dao/TcDataDirectory.java
@@ -2,6 +2,7 @@ package com.ysu.zyw.tc.api.dao;
 
 public interface TcDataDirectory {
 
+    /** -------------------------------------------------- region -------------------------------------------------- */
     String REGION_CHN = "CHN";
 
 }
