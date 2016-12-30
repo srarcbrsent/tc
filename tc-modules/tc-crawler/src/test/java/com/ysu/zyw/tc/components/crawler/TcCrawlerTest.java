0591a0639ecd1e776e452dbdae7be35ec7fae7eb
张耀武 <zhang_yao_wu1993@yeah.net>
2016-12-30 16:57:36 +0800

    optimize (188)

---
+++ tc-modules/tc-crawler/src/test/java/com/ysu/zyw/tc/components/crawler/TcCrawlerTest.java
@@ -0,0 +1,55 @@
+package com.ysu.zyw.tc.components.crawler;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TcCrawlerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void test() throws Exception {
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://www.oschina.net");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(TcCrawler.class, numberOfCrawlers);
+    }
+
+}
