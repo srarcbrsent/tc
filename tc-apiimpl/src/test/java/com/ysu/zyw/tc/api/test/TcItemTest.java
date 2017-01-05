ff0ad300ba5e3c31399ffe03b0b3a99428ca57da
张耀武 <zhang_yao_wu1993@yeah.net>
2017-01-05 22:42:46 +0800

    project optimize (299)

--- tc-apiimpl/src/test/java/com/ysu/zyw/tc/api/test/TcItemTest.java
+++ tc-apiimpl/src/test/java/com/ysu/zyw/tc/api/test/TcItemTest.java
@@ -3,13 +3,16 @@ package com.ysu.zyw.tc.api.test;
 import com.ysu.zyw.tc.api.dao.mappers.TcItemMapper;
 import com.ysu.zyw.tc.api.svc.items.TcItemService;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -19,6 +22,10 @@ import javax.annotation.Resource;
 })
 @Slf4j
 public class TcItemTest {
+
+    private static final String ES_INDEX = "demo_index";
+
+    private static final String ES_TYPE = "demo_doc";
 
     @Resource
     private TcItemService tcItemService;
@@ -30,7 +37,23 @@ public class TcItemTest {
     private TransportClient transportClient;
 
     @Test
-    public void testBySpringDataES() {
+    public void testES() {
+//        TcItem tcItem = tcItemMapper.selectByPrimaryKey("00002D472C7B48D896A48B5F5FE0A522");
+//        IndexResponse indexResponse = transportClient.prepareIndex(ES_INDEX, ES_DOCUMENT, "00002D472C7B48D896A48B5F5FE0A522")
+//                .setSource(TcSerializationUtils.writeJson(tcItem))
+//                .get();
+//        System.out.println(indexResponse.getId());
+//        System.out.println(indexResponse.getIndex());
+//        System.out.println(indexResponse.getType());
+//        System.out.println(indexResponse.getVersion());
+//        System.out.println(indexResponse.getResult());
+        SearchResponse searchResponse = transportClient.prepareSearch(ES_INDEX)
+                .setTypes(ES_TYPE)
+                .setQuery(QueryBuilders.matchQuery("shopId", "A1861BD5CCE44B399E891DAAD99BAE27"))
+                .get();
+        System.out.println(searchResponse.getHits().getHits().length);
+        Arrays.stream(searchResponse.getHits().getHits())
+                .forEach(searchHitFields -> System.out.println(searchHitFields.getId()));
     }
 
 }
