6a06f4b42d1c762786767f03e28fb8d1159ee70f
张耀武 <zhang_yao_wu1993@yeah.net>
2017-01-15 21:25:52 +0800

    project optimize (316)

---
+++ tc-modules/tc-sch-elasticjob/src/test/java/com/ysu/zyw/tc/components/sch/elasticjob/test/TcSimpleJob.java
@@ -0,0 +1,13 @@
+package com.ysu.zyw.tc.components.sch.elasticjob.test;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+
+public class TcSimpleJob implements SimpleJob {
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+
+    }
+
+}
