31827713cb4d0e15e2db6f9929f184e1b554fddc
张耀武 <zhang_yao_wu1993@yeah.net>
2017-01-16 15:13:51 +0800

    optimize (325)

--- tc-openapi/src/main/java/com/ysu/zyw/tc/openapi/fk/shiro/enhance/TcAbstractRequiresRolesDynamicFilter.java
+++ tc-openapi/src/main/java/com/ysu/zyw/tc/openapi/fk/shiro/enhance/TcAbstractRequiresRolesDynamicFilter.java
@@ -1,29 +1,24 @@
 package com.ysu.zyw.tc.openapi.fk.shiro.enhance;
 
 import com.ysu.zyw.tc.openapi.fk.shiro.support.TcRequiresRolesDynamicSup;
-import org.apache.shiro.authz.annotation.Logical;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.web.filter.AccessControlFilter;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class TcRequiresRolesDynamicFilter extends AccessControlFilter {
+@Slf4j
+public abstract class TcAbstractRequiresRolesDynamicFilter extends AccessControlFilter {
 
     @Resource
-    private TcRequiresRolesDynamicSup tcRequiresRolesDynamicSup;
-
-    @Override
-    protected boolean isAccessAllowed(ServletRequest request,
-                                      ServletResponse response,
-                                      Object mappedValue) throws Exception {
-        return tcRequiresRolesDynamicSup.isAccessAllowed((String[]) mappedValue, Logical.AND);
-    }
+    protected TcRequiresRolesDynamicSup tcRequiresRolesDynamicSup;
 
     @Override
     protected boolean onAccessDenied(ServletRequest request,
                                      ServletResponse response) throws Exception {
         saveRequestAndRedirectToLogin(request, response);
+        log.info("reject request and redirect to login [{}]", request);
         return true;
     }
 
---
+++ tc-openapi/src/main/java/com/ysu/zyw/tc/openapi/fk/shiro/enhance/TcRequiresAllRolesDynamicFilter.java
@@ -0,0 +1,16 @@
+package com.ysu.zyw.tc.openapi.fk.shiro.enhance;
+
+import org.apache.shiro.authz.annotation.Logical;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class TcRequiresAllRolesDynamicFilter extends TcAbstractRequiresRolesDynamicFilter {
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue)
+            throws Exception {
+        return tcRequiresRolesDynamicSup.isAccessAllowed((String[]) mappedValue, Logical.AND);
+    }
+
+}
