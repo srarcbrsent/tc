92b3e0d816183ddf1671cd2cf59efe99eae42d06
张耀武 <zhang_yao_wu1993@yeah.net>
2017-01-17 14:55:22 +0800

    optimize (331)

--- tc-modules/tc-spring-reactive/src/test/java/com/ysu/zyw/tc/components/reactive/TcReactiveDemos.java
+++ tc-modules/tc-spring-reactive/src/test/java/com/ysu/zyw/tc/components/reactive/TcReactiveDemos.java
@@ -5,18 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
 @Slf4j
 public class TcReactiveDemos {
 
     @Test
     public void test01() {
         Flux.fromIterable(Lists.newArrayList(1L, 2L, 3L, 4L))
-                .mergeWith(Flux.interval(Duration.of(1000, ChronoUnit.MILLIS)))
-                .map(d -> d * 2)
-                .take(3)
+                .parallel(5)
+                .map(Math::sqrt)
+                .log()
+                .doOnNext(ele -> System.out.println(Thread.currentThread()))
                 .subscribe(System.out::println);
     }
 
