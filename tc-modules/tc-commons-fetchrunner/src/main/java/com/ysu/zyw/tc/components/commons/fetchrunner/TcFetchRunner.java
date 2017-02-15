8d347f9abb554f43c0fab6204449445f0e0e2203
张耀武 <zhang_yao_wu1993@yeah.net>
2017-02-15 13:47:43 +0800

    optimize (333)

--- tc-modules/tc-commons-fetchrunner/src/main/java/com/ysu/zyw/tc/components/commons/fetchrunner/TcFetchRunner.java
+++ tc-modules/tc-commons-fetchrunner/src/main/java/com/ysu/zyw/tc/components/commons/fetchrunner/TcFetchRunner.java
@@ -1,7 +1,7 @@
 package com.ysu.zyw.tc.components.commons.fetchrunner;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
 
@@ -10,13 +10,25 @@ import java.util.List;
  * 跳过策略 不跳过 基于zk锁跳过
  * 异常处理策略 日志处理 错误直接退出
  * 连接池管理(fetch线程和run线程)
+ *
  * @param <T>
  */
+@Slf4j
 public abstract class TcFetchRunner<T> {
 
-    @Getter
-    @Setter
-    private TcSkipStrategy tcSkipStrategy = TcNonSkipStrategy.INSTANCE;
+    public void go() {
+        log.info("fetch runner prepare to go ...");
+        try {
+            List<T> datas = fetch();
+            while (CollectionUtils.isNotEmpty(datas)) {
+                run(datas);
+                datas = fetch();
+            }
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        log.info("fetch runner reach end ...");
+    }
 
     public abstract List<T> fetch();
 
