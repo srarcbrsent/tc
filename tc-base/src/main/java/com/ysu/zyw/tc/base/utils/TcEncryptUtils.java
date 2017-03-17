31cd97f3b2d12961df933c1ba7f1f3aec44d4fa4
张耀武 <zhang_yao_wu1993@yeah.net>
2017-03-18 00:15:54 +0800

    add encrypt utils(rsa)

---
+++ tc-base/src/main/java/com/ysu/zyw/tc/base/utils/TcEncryptUtils.java
@@ -0,0 +1,51 @@
+package com.ysu.zyw.tc.base.utils;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nonnull;
+import javax.crypto.Cipher;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@Slf4j
+@UtilityClass
+public class TcEncryptUtils {
+
+    private static final String RSA_ALGORITHM = "RSA";
+
+    private static final int RSA_ALGORITHM_KEY_SIZE = 1024;
+
+    @SneakyThrows
+    public static KeyPair generate() {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        keyPairGenerator.initialize(RSA_ALGORITHM_KEY_SIZE);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    @SneakyThrows
+    public static String encryptRSA(@Nonnull PublicKey publicKey, @Nonnull String text) {
+        checkNotNull(publicKey);
+        checkNotNull(text);
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] cipherBytes = cipher.doFinal(text.getBytes());
+        return new String(cipherBytes);
+    }
+
+    @SneakyThrows
+    public static String decryptRSA(@Nonnull PrivateKey privateKey, @Nonnull String text) {
+        checkNotNull(privateKey);
+        checkNotNull(text);
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] plainBytes = cipher.doFinal(text.getBytes());
+        return new String(plainBytes);
+    }
+
+}
--- tc-openapi/src/main/java/com/ysu/zyw/tc/openapi/fk/config/TcConfig.java
+++ tc-openapi/src/main/java/com/ysu/zyw/tc/openapi/fk/config/TcConfig.java
@@ -1,7 +1,9 @@
 package com.ysu.zyw.tc.openapi.fk.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 public class TcConfig {
 
