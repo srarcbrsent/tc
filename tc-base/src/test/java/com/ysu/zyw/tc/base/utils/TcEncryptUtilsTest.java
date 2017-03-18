8e83310e0a460c184a077d7e26e1584af0cf8cce
张耀武 <zhang_yao_wu1993@yeah.net>
2017-03-18 22:04:21 +0800

    complete rsa encrypt utils

---
+++ tc-base/src/test/java/com/ysu/zyw/tc/base/utils/TcEncryptUtilsTest.java
@@ -0,0 +1,38 @@
+package com.ysu.zyw.tc.base.utils;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.security.KeyPair;
+
+public class TcEncryptUtilsTest {
+
+    @Test
+    public void testRSA() {
+        String plain = RandomStringUtils.randomAscii(40);
+
+        KeyPair keyPair = TcEncryptUtils.generateKeyPair();
+        String publicRSAKey = TcEncryptUtils.getPublicRSAKey(keyPair);
+        String privateRSAKey = TcEncryptUtils.getPrivateRSAKey(keyPair);
+
+        System.out.println(publicRSAKey);
+        System.out.println(privateRSAKey);
+        System.out.println();
+
+        // encrypt
+        String encrypt = TcEncryptUtils.encrypt(plain, publicRSAKey);
+        System.out.println(encrypt);
+        String decrypt = TcEncryptUtils.decrypt(encrypt, privateRSAKey);
+        System.out.println(decrypt);
+        Assert.assertEquals(plain, decrypt);
+
+        // sign
+        String sign = TcEncryptUtils.sign(plain, privateRSAKey);
+        System.out.println(sign);
+        String verify = TcEncryptUtils.verify(sign, publicRSAKey);
+        System.out.println(verify);
+        Assert.assertEquals(plain, verify);
+    }
+
+}
