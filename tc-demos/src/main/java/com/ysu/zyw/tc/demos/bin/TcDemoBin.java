c9654aa2a3dc98689cef83e0022f859a95f0fd0d
张耀武 <zhang_yao_wu1993@yeah.net>
2017-04-12 22:40:59 +0800

    reverse string

--- tc-demos/src/main/java/com/ysu/zyw/tc/demos/bin/TcDemoBin.java
+++ tc-demos/src/main/java/com/ysu/zyw/tc/demos/bin/TcDemoBin.java
@@ -10,6 +10,10 @@ public class TcDemoBin {
         hexadecimal10To26(1);
         System.out.println();
         hexadecimal10To26(22222);
+        System.out.println();
+        reverse("i am a boy");
+        System.out.println();
+        reverse(" boy am i");
     }
 
     private static void hexadecimal10To26(int num) {
@@ -17,6 +21,19 @@ public class TcDemoBin {
             hexadecimal10To26(num / 26);
             System.out.print((char) ('A' + num % 26));
         }
+    }
+
+    private static void reverse(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int before = str.length() - 1, after = str.length(); before >= 0; before--) {
+            if (str.charAt(before) == ' ') {
+                sb.append(str.substring(before + 1, after)).append(" ");
+                after = before;
+            } else if (before == 0) {
+                sb.append(str.substring(0, after));
+            }
+        }
+        System.out.println(sb.toString());
     }
 
 }
