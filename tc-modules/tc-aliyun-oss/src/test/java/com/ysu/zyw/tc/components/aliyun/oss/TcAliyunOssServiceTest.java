591351c4472b27925823f6b2da5fa5c8c696c9d4
张耀武 <zhang_yao_wu1993@yeah.net>
2017-04-15 09:49:46 +0800

    aliyun oss refactor

--- tc-modules/tc-aliyun-oss/src/test/java/com/ysu/zyw/tc/components/aliyun/oss/TcAliyunOssServiceTest.java
+++ tc-modules/tc-aliyun-oss/src/test/java/com/ysu/zyw/tc/components/aliyun/oss/TcAliyunOssServiceTest.java
@@ -2,11 +2,14 @@ package com.ysu.zyw.tc.components.aliyun.oss;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -16,17 +19,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @Slf4j
 public class TcAliyunOssServiceTest {
 
+    private static final String DEFAULT_BUCKET = "tc-resources";
+
+    private static final String DEMO_FOLDER = "/helloworld";
+
+    @Resource
+    private TcAliyunOssService tcAliyunOssService;
+
     @Before
     public void setUp() throws Exception {
+        tcAliyunOssService.set(DEFAULT_BUCKET, DEMO_FOLDER);
     }
 
     @After
     public void tearDown() throws Exception {
+        tcAliyunOssService.del(DEFAULT_BUCKET, DEMO_FOLDER);
     }
 
     @Test
     public void test() {
-
+        Assert.assertTrue(tcAliyunOssService.exists(DEFAULT_BUCKET, DEMO_FOLDER));
     }
 
 }
